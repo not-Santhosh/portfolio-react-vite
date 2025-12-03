cdb1c6da8b65510559f05c905d8e9f9c0f002c9f
santhosh <santhosh@awp.dev>
2025-12-03 06:02:49 +0530

    chore: delete numerous dependency files from node_modules and create .gitignore.

--- vite-project/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
